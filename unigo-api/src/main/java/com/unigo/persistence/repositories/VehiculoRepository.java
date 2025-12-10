16e2719c5289a4bf38f94a71b5a1c68fdf49948b
Miguel González del Portillo <miguelgp2005@gmail.com>
2025-12-10 18:19:08 +0100

    Vehiculo get y post Cruds con conductor auto create. También conductor ADMIN create.
    TODO: GlobalHandlerException

--- unigo-api/src/main/java/com/unigo/persistence/repositories/VehiculoRepository.java
+++ unigo-api/src/main/java/com/unigo/persistence/repositories/VehiculoRepository.java
@@ -4,12 +4,9 @@ import com.unigo.persistence.entities.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Integer> {
     boolean existsByIdAndIdConductor(int idVehiculo, int idConductor);
 
     List<Vehiculo> findAllByIdConductor(int idConductor);
-
-    Optional<Vehiculo> findByIdAndIdConductor(int id, int idConductor);
 }
